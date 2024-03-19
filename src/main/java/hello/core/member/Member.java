665fb089bcf10bbeb407f274ac6a928d190ae6a6
jinhyeon <tiemo0708@gmail.com>
2024-03-19 13:58:20 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/hello/core/member/Member.java
@@ -0,0 +1,15 @@
+package hello.core.member;
+
+public class Member {
+    private Long id;
+    private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+
+}
